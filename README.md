2f1c7b83c40025f1838ae6b92112783ef904fe35
Rina-01 <ira.mashkova.2001@mail.ru>
2023-12-07 22:50:21 +0300

    iter1
    базовые поля таблиц

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # College
+
+browser
+http://127.0.0.1:8000/
+
+
+Django Tutorial
+part 1 - https://docs.djangoproject.com/en/5.0/intro/tutorial01/
+part 2 - https://docs.djangoproject.com/en/5.0/intro/tutorial02/
+
+college\settings.py
+- LANGUAGE_CODE
+- TIME_ZONE
+
+
+Django типы данных в Model
+https://docs.djangoproject.com/en/4.2/ref/models/fields/
+
+BD:
+- тип PositiveSmallIntegerField для полей semester_num, duration
+- в Final_grades изменены имена полей (нельзя имя начинать с цифры!)
+- обдумать, возможно для scale_5, scale_letter и scale_word надо просто задать набор возможных значений
+- тоже самое и для lesson_type в Curriculum_lesson
+
+
--- college/polls/models.py
+++ college/polls/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Students(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    second_name = models.CharField(max_length=128, blank=True)
+    date_of_birth = models.DateField()
+
+class Groups(models.Model):
+    name = models.CharField(max_length=16)
+
+class Disciplines(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=1000)
+
+class Titors(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    second_name = models.CharField(max_length=128, blank=True)
+    date_of_birth = models.DateField()
+
+class Group_semesters(models.Model):
+    semester_num = models.PositiveSmallIntegerField()
+
+# class Group_members(models.Model):
+
+class Curriculum(models.Model):
+    duration = models.PositiveSmallIntegerField()
+
+class Final_grades(models.Model):
+    # semester_num = models.PositiveSmallIntegerField()
+    is_final = models.BooleanField()
+    scale_5 = models.PositiveSmallIntegerField()
+    scale_100 = models.PositiveSmallIntegerField()
+    scale_letter = models.CharField(max_length=1)
+    scale_word = models.CharField(max_length=128)
+
+
+class Curriculum_lesson(models.Model):
+    lesson_type = models.PositiveSmallIntegerField()
+
+
